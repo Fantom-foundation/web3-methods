543c760954b0d55467e0871dde494e10f81f2524
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2022-05-16 12:36:56 +0400

    Add web3.subscribe (in progress)

---
+++ scripts/web3.eth.subscribe.js
@@ -0,0 +1,30 @@
+var Web3 = require('web3');
+
+const { RPC, ABI, CONTRACT_ADDRESS, TOPIC0 } = require('../constants');
+
+const localProvider = new Web3.providers.WebsocketProvider(
+  'wss://rpc.testnet.fantom.network'
+);
+const web3 = new Web3(localProvider);
+
+let options = {
+  address: CONTRACT_ADDRESS,
+  topics: [TOPIC0]
+};
+
+const web3Subscribe = async () => {
+  var subscription = web3.eth
+    .subscribe('logs', options, function (error, result) {
+      if (!error) console.log(result);
+      else console.error(error);
+    })
+    .on('connected', function (subscriptionId) {
+      console.log(subscriptionId);
+    })
+    .on('data', function (log) {
+      console.log(log);
+    })
+    .on('changed', function (log) {});
+};
+
+web3Subscribe();
